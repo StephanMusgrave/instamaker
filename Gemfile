7fac4db053602bb124fc8f5790f0d96db66035ce
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-05-28 17:59:02 +0100

    AWS server config sorted

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'spring',        group: :development
 gem 'devise'
 
 gem "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
-
+gem 'aws-sdk'
 gem 'bootstrap-sass'
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 3 }
-  has_attached_file :picture, styles: { medium: '300x300>' }
-  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :picture, 
+    styles: { medium: '300x300>' },
+    storage: :s3,
+    s3_host_name: 's3-us-west-2.amazonaws.com',
+    s3_credentials: {
+      bucket: 'instamaker',
+      access_key_id: Rails.application.secrets.s3_access_key,
+      secret_access_key: Rails.application.secrets.s3_secret_key
+     }
+    validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
 
   # has_many  :tags
