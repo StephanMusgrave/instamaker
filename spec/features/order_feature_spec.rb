a5a75ea540c18fedb27f149d53eef8e41916bf15
Steve Musgrave <steve.musgrave@outlook.com>
2014-06-03 18:19:22 +0100

    order feature test in progress

--- spec/features/order_feature_spec.rb
+++ spec/features/order_feature_spec.rb
@@ -1,9 +1,29 @@
-order_feature_spec.rb
-
 require 'spec_helper'
 
-describe 'orders' do
-  it '' do
-    
+describe 'orders page' do
+  let(:post) { create(:post) }
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
+
+  context 'logged in as admin' do
+    context 'no orders' do
+      it 'sees a message' do
+        visit '/orders'
+        expect(page).to have_content 'No orders yet'
+      end
+    end
   end
+  
+  context 'not logged in as admin' do
+      it 'prompts to sign in ' do
+        visit '/orders'
+        expect(page).to have_content 'sign in'
+      end
+  end
+
+  # before do
+  #   Order.create(post: post,)
+  # end
+
+  
 end
